590fa3f63b65db999127cafd3086f75fc81852ad
Artur <artur_avagyan2@edu.aua.am>
2018-09-13 21:54:16 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,10 @@
+<script>
+    const a = 54
+	const b = 45
+	const c = "homewoork"
+	if (a ===54 && b >= 50)
+		console.log ("yuppy")
+		else if (a >= 60 || c === "homewoork")
+			console.log ("woow") 
+			else console.log ("no way")
+</script>
